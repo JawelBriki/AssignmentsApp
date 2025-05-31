27b58e0732597a7522f4c99dbc1c88bbf96cb080
Jawel Briki <jawel.briki@etu.univ-cotedazur.fr>
2025-05-31 02:54:22 +0200

    upgrade add assignment form

--- src/app/assignments/add-assignment/add-assignment.component.ts
+++ src/app/assignments/add-assignment/add-assignment.component.ts
@@ -1,56 +1,74 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core'
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 import { Assignment } from '../assignment.model';
 import { AssignmentsService } from '../../shared/assignments.service';
 import { Router } from '@angular/router';
 
 @Component({
-  providers: [provideNativeDateAdapter()],
+  providers: [],
   selector: 'app-add-assignment',
-  imports: [FormsModule, MatInputModule, MatDatepickerModule, 
-    MatButtonModule, MatFormFieldModule],
+  imports: [FormsModule, MatInputModule, MatDatepickerModule,
+    MatButtonModule, MatFormFieldModule, MatSelectModule,
+    MatNativeDateModule, CommonModule],
   templateUrl: './add-assignment.component.html',
   styleUrl: './add-assignment.component.css'
 })
-export class AddAssignmentComponent  {
-  // Pour le formulaire d'ajout
-  nomDevoir = "";
-  dateDeRendu!:Date;
+export class AddAssignmentComponent implements OnInit {
 
-  constructor(private assignmentsService:AssignmentsService, 
-              private router:Router) {}
+  assignmentName = "";
+  dueDate!: Date;
+  selectedSubject = "";
+  author = "";
 
-  
-onSubmit(event:any) {
-    console.log(`On a soumis le formulaire nom = ${this.nomDevoir}, 
-      dateDeRendu = ${this.dateDeRendu}`);
+  // Predefined subjects
+  subjects: string[] = [
+    "Web Development", "Data Structures and Algorithms", "Introduction to Computer Science",
+    "Software Engineering", "Operating Systems", "Artificial Intelligence",
+    "Computer Graphics", "Computer Networks", "Database Management", "Cybersecurity"
+  ];
 
-      // On ne crée un nouvel assignment que si le formulaire est valide
-      // c'est-à-dire si le nom du devoir n'est pas vide et si la date de rendu est bien définie
-      if(this.nomDevoir == "" || this.dateDeRendu == null) {
-        console.log("Formulaire invalide");
-        return;
-      }
+  constructor(private assignmentsService: AssignmentsService,
+              private router: Router) {}
 
-      let a = new Assignment();
-      a.nom = this.nomDevoir;
-      a.dateDeRendu = this.dateDeRendu;
-      a.rendu = false;
+  ngOnInit(): void {}
 
-      // On envoie l'assignment vers le service pour insertion
-      this.assignmentsService.addAssignment(a)
+  onSubmit(event: any) {
+    console.log("Form submitted");
+
+    // Validate the form
+    if (this.assignmentName === "" || this.dueDate == null || this.selectedSubject === "" || this.author === "") {
+      console.log("Invalid form");
+      console.log(this.assignmentsService.getNewAssignmentID());
+      return;
+    }
+
+    // Create a new assignment
+    let a = new Assignment();
+    console.log("NEW ASSIGNMENT ID = ", this.assignmentsService.getNewAssignmentID())
+    a._id = this.assignmentsService.getNewAssignmentID();
+    a.nom = this.assignmentName;
+    a.dateDeRendu = this.dueDate;
+    a.subject = this.selectedSubject;
+    a.author = this.author;
+    a.rendu = false;
+
+    // Update the assignment ID tracker
+    this.assignmentsService.setNewAssignmentID(a._id + 1)
+
+    // Send the assignment to the service for insertion
+    this.assignmentsService.addAssignment(a)
       .subscribe(message => {
         console.log(message);
 
-       // On va naviguer vers la page qui affiche la liste des assignments
-       // c'est la route par défaut (/ ou /home)
-       this.router.navigate(['/home']);
+        // Navigate to the assignments list
+        this.router.navigate(['/home']);
       });
   }
-
 }
